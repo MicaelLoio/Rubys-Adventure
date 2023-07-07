6e11010855a57d633ed565a7e3b6cece5b6a5bbd
MicaelLoio <131375089+MicaelLoio@users.noreply.github.com>
2023-07-07 03:13:31 +0100

    Sprite Animation

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -11,12 +11,26 @@ using UnityEngine;
     Rigidbody2D rigidbody2D;
     float timer;
     int direction = 1;
+    Animator animator;
+    
+﻿public class EnemyController2 : MonoBehaviour
+{
+    public float speed;
+    public bool vertical;
+    public float changeTime = 3.0f;
+
+    Rigidbody2D rigidbody2D;
+    float timer;
+    int direction = 1;
+    
+    Animator animator;
     
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -36,16 +50,20 @@ using UnityEngine;
         
         if (vertical)
         {
-            position.y = position.y + Time.deltaTime * speed * direction;;
+            position.y = position.y + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
         }
         else
         {
-            position.x = position.x + Time.deltaTime * speed * direction;;
+            position.x = position.x + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
         }
         
         rigidbody2D.MovePosition(position);
     }
-
+    
     void OnCollisionEnter2D(Collision2D other)
     {
         RubyController player = other.gameObject.GetComponent<RubyController >();
