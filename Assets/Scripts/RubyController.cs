c3d5b0b7064f196b197d07bda2be674ebd71ce1d
MicaelLoio <131375089+MicaelLoio@users.noreply.github.com>
2023-07-08 02:12:57 +0100

    Visual Styling - User Interface - Head-Up Display
    Creating the UI health Bar

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -85,7 +85,8 @@ using UnityEngine;
         }
         
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
+        
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
     }
     
     void Launch()
